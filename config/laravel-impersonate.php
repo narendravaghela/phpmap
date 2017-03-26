d14d9f4f4bdda7df0e24fc56f7984cddf70b703d
Florian Wartner <f.wartner@icloud.com>
2017-03-26 08:45:53 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/laravel-impersonate.php
+++ config/laravel-impersonate.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-    
-    /**
+
+    /*
      * The session key used to store the original user id.
      */
     'session_key' => 'impersonated_by',
 
-    /**
+    /*
      * The URI to redirect after taking an impersonation.
      *
      * Only used in the built-in controller.
      */
     'take_redirect_to' => '/',
 
-    /**
+    /*
      * The URI to redirect after leaving an impersonation.
      *
      * Only used in the built-in controller.
